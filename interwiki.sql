51383f5f6a41566507abbc16af5fed8d6302d0e2
Sheldon Rampton <sheldon_rampton@13085.no-reply.drupal.org>
2005-01-01 00:49:50 +0000

    adding documentation, administration features; fixed a couple of bugs

--- interwiki.sql
+++ interwiki.sql
@@ -7,7 +7,7 @@ CREATE TABLE interwiki (
   UNIQUE KEY iw_prefix (iw_prefix)
 );
 
-INSERT INTO interwiki values ('kos','http://www.dkosopedia.com/index.php/',0);
-INSERT INTO interwiki values ('w','http://en.wikipedia.org/wiki/',0);
+INSERT INTO interwiki values ('kos','http://www.dkosopedia.com/index.php/$1',0);
+INSERT INTO interwiki values ('w','http://en.wikipedia.org/wiki/$1',0);
 INSERT INTO interwiki values ('dis','http://www.disinfopedia.org/wiki.phtml?title=',0);
-INSERT INTO interwiki values ('vp','http://www.voterpunch.org/category.jsp?issue=',0);
+INSERT INTO interwiki values ('vp','http://www.voterpunch.org/category.jsp?issue=$1',0);
