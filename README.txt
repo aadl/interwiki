f78ee3190d65b5c99bc8f4d3a16dc5737035a7d7
Neil Drumm <drumm@3064.no-reply.drupal.org>
2005-02-17 20:02:37 +0000

    Oops, meant to get HEAD updated first. Finishing the name change.

--- README.txt
+++ README.txt
@@ -1,5 +1,13 @@
-This filter allows you to use Media Wiki (such as Wikipedia, dKosopedia, etc)
-links.  It uses a table interwiki which is the same table that Media Wiki uses,
-so you should be able to use an interwiki table interchangably.
+This filter allows you to use interwiki links that point to wikis
+such as Wikipedia.org, SourceWatch.org and dKosopedia.com. It can also be
+used to link easily to Google and eBay searches and other online reference
+sources such as the Merriam-Webster dictionary. It uses a table interwiki 
+which is the same table used by <a href="http://wikipedia.sourceforge.net/">MediaWiki</a>,
+so MediaWiki users should be able to use an interwiki table interchangeably.
 
-Questions?  Contact Aldon Hynes ahynes1@optonline.net
+Wikipedia has an article with further information about interwikis, which
+can be found at:
+http://en.wikipedia.org/wiki/Interwiki
+
+Questions?
+Contact Aldon Hynes <ahynes1@optonline.net> or Sheldon Rampton <sheldon@prwatch.org>.
--- interwiki.sql
+++ interwiki.sql
@@ -7,14 +7,7 @@ CREATE TABLE interwiki (
   UNIQUE KEY iw_prefix (iw_prefix)
 );
 
-INSERT INTO interwiki values ('kos','http://www.dkosopedia.com/index.php/$1',0);
-INSERT INTO interwiki values ('w','http://en.wikipedia.org/wiki/$1',0);
-INSERT INTO interwiki values ('dis','http://www.disinfopedia.org/wiki.phtml?title=$1',0);
-INSERT INTO interwiki values ('pp','http://www.progressivepunch.org/export/plugin?profile=blog&searchString=$1',0);
-INSERT INTO interwiki values ('http','http:$1',0);
-INSERT INTO interwiki values ('','$1',1);
-INSERT INTO interwiki values ('googledef','http://www.google.com/search?&q=define:$1',0);
-INSERT INTO interwiki values ('dict','http://www.m-w.com/cgi-bin/dictionary?book=Dictionary&va=$1',0);
-INSERT INTO interwiki values ('google','http://www.google.com/search?q=%22$2%22',0);
-INSERT INTO interwiki values ('ebay','http://search.ebay.com/$4',0);
-INSERT INTO interwiki values ('contact','http://contacts.smartcampaigns.com/index.php?title=$1',1);
+INSERT INTO interwiki values ('kos','http://www.dkosopedia.com/index.php/',0);
+INSERT INTO interwiki values ('w','http://en.wikipedia.org/wiki/',0);
+INSERT INTO interwiki values ('dis','http://www.disinfopedia.org/wiki.phtml?title=',0);
+INSERT INTO interwiki values ('vp','http://www.voterpunch.org/category.jsp?issue=',0);
